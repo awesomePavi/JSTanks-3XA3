dfb57b2d4252bc7fc74c51b4c5cd33e2ec1960b0
patelvh3 <patelvh3@mcmaster.ca>
2016-10-25 16:44:55 -0400

    deleted unneccessary code in Tank.js

--- src/JavaScript/Tank.js
+++ src/JavaScript/Tank.js
@@ -4,38 +4,7 @@ Author: Viren Patel
 
 */
 
-//import the tank images
-var pic = new Image();
-pic.src = "../Images/tank.jpg";
-
-// function to take in the keyboard input
-keyPressed = function(){
-	
-}
-
-/*
-// Tank object constructor and definition
-var Tank = function (tileSize){
-	this.moveSize = tileSize;
-	this.posX = tileSize*5;
-	this.posY = tileSize*5;
-	this.attack = false; 
-	this.health = 100;
-	this.currentImage = tankImage; 
-	
-	
-}
-*/
-
-/*
-Tank.prototype.draw = function (x, y, drawImage){
-	var ctx = document.getElementById('Game').getContext('2d');
-	//drawImage(picture, X, Y)
-	//drawImage(picture, X, Y, width, height)
-	ctx.drawImage(pic, 100, 100);
-}
-*/
-
+// Tank constructor and definition
 var Tank = function (tileSize){
 	this.m_canvas = document.createElement('canvas');
 	this.m_canvas.width = tileSize;
@@ -49,6 +18,11 @@ var Tank = function (tileSize){
    	}
 }
 
+// method to draw the tank image - called by GameBoard
+Tank.prototype.draw = function(canvas,startx,startY,tileSize){
+	canvas.drawImage(this.m_canvas,startx,startY);
+}
+
 // function to determine if the tank will take damage or not
 Tank.prototype.damage = function(x, y){
 	
@@ -58,27 +32,6 @@ Tank.prototype.damage = function(x, y){
 Tank.prototype.update = function(){
 	this.updatePos();
 	this.updateImage();
-}
-
-// function to update the tank's position according to the input
-Tank.prototype.updatePos = function(){	
-		if (keyCode === UP){
-			this.posY += this.moveSize;
-		}
-		if (keyCode === DOWN){
-			this.posY -= this.moveSize;
-		}
-		if (keyCode === LEFT){
-			this.posX -= this.moveSize;
-		}
-		if (keyCode === RIGHT){
-			this.posY += this.moveSize;
-		}	
-}
-
-// function to update the image of the tank (full health, damaged, destroyed,..)
-Tank.prototype.updateImage = function(){
-	
 }
 
 //
@@ -98,10 +51,7 @@ Tank.prototype.getPosition = function (){
 	return this.x, this.y;
 }
 
-Tank.prototype.draw = function(canvas,startx,startY,tileSize){
-	canvas.drawImage(this.m_canvas,startx,startY);
-	
-}
+
 
 
 	
